4c6182f41e5b4f9db961bb9b478f041ed8299e20
Fatima Zehra <fz8585949@gmail.com>
2024-02-09 23:47:18 +0500

    Developed responsive home page

--- app/page.jsx
+++ app/page.jsx
@@ -2,112 +2,67 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col">
+      <div className="z-10 w-full flex flex-row justify-center items-center mt-5 xl:mt-9 font-sans">
+        <h4 className="font-lato text-base xl:text-xl font-semibold">
+          SHARED PLATE
+        </h4>
+      </div>
+
+      <div className="absolute top-5 xl:top-8 right-5 xl:right-9 w-full flex flex-row justify-end items-center gap-5">
+        <h4 className="hidden xl:block font-notoSans text-xl">
+          Hi! Fatima Zehra
+        </h4>
+        <div className="text-white bg-black rounded-full flex justify-center items-center font-notoSans text-base xl:text-2xl h-7 w-7 xl:h-10 xl:w-10 xl:mr-9">
+          A
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div className="flex flex-col my-5 xl:m-9">
+        <div className="flex justify-center xl:block">
+          <Image
+            src="/Assets/foodWaste.svg"
+            alt="Food Waste"
+            className="w-[110px] h-[110px] xl:w-[400px] xl:h-[400px] rounded-full drop-shadow-[8px_8px_3px_theme('colors.red')] xl:rounded-none xl:absolute xl:top-0 xl:left-0 xl:drop-shadow-[24px_24px_0px_theme('colors.red')]"
+            width={400}
+            height={400}
+            priority
+          />
+        </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <h1 className="font-notoSans font-bold text-center mb-5 text-5xl mt-5 animate-zoomIn xl:mb-0 xl:mx-[410px] xl:text-8xl leading-tight xl:leading-normal">
+          <span className="text-red">Waste</span> Less, <br />{" "}
+          <span className="text-green">Give</span> More
+        </h1>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <div className="flex justify-center xl:block">
+          <Image
+            src="/Assets/donateFood.svg"
+            alt="Food Donation"
+            className="w-[110px] h-[110px] rounded-full drop-shadow-[8px_8px_3px_theme('colors.green')] xl:w-[400px] xl:h-[400px] xl:rounded-none xl:absolute xl:bottom-0 xl:right-0 xl:drop-shadow-[-24px_-24px_0px_theme('colors.green')]"
+            width={400}
+            height={400}
+            priority
+          />
+        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+        <p className="font-notoSans font-medium text-center text-sm mt-6 mb-6 mx-8 xl:text-xl xl:mt-12 xl:mb-8 xl:mx-[410px]">
+          Our platform connects generous hearts with those in need, creating a
+          seamless bridge to combat hunger and reduce food waste. Join us in our
+          mission to foster a community of sharing and kindness, one meal at a
+          time. Together, let's make a meaningful impact and nourish lives with
+          every click.
+        </p>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="flex flex-row justify-center mt-2 gap-8 mx-4 xl:mt-4 xl:ml-9 xl:gap-16">
+          <button className="bg-black border h-8 w-24 xl:h-12 xl:w-28 text-white font-notoSans font-semibold text-base xl:text-xl transition-colors hover:border-black hover:bg-green">
+            Give
+          </button>
+          <button className="bg-black border h-8 w-24 xl:h-12 xl:w-28 text-white font-notoSans font-semibold text-base xl:text-xl transition-colors hover:border-black hover:bg-green">
+            Deliver
+          </button>
+        </div>
       </div>
     </main>
-  )
+  );
 }
