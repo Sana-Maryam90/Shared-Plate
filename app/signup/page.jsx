99e95223d98a3f0bd5cbba8815aa1d88e14cade3
Fatima Zehra <fz8585949@gmail.com>
2024-02-14 21:55:46 +0500

    Separate client and server rendering of login and signUp pages

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,77 +1,8 @@
-"use client";
-import InputField from "../components/InputField";
 import Link from "next/link";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
-import { toast } from "react-toastify";
 import Toast from "../components/Toast";
-import axios from "axios";
+import SignUpForm from "../components/SignUpForm";
 
 export default function SignUp() {
-
-  // States for setting form data
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isChecked, setIsChecked] = useState(false);
-
-  const router = useRouter();
-
-  const handleCheckboxChange = () => {
-    setIsChecked(!isChecked);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    // Form Data Validation
-    if (!name || !email || !password || isChecked == false) {
-      toast.error("Please fill in all fields");
-    } else if (!email.includes("@") || !email.includes(".")) {
-      toast.error("Please enter a valid email address");
-    } else if (password.length < 6) {
-      toast.error("Password should be atleast 6 character long");
-    } else {
-
-      // Post Data using axios
-      const signUpData = {
-        email,
-        name,
-        password,
-      };
-      console.log("Sign Up Data: ", signUpData);
-      try {
-        const response = await axios({
-          url: "",
-          method: "POST",
-          data: signUpData,
-        });
-
-        console.log("Registration Successfull Response: ", response.message);
-        toast.success("Registration successful");
-        
-        // Navigate to login Page
-        setTimeout(() => {
-          router.push("/login");
-        }, 1000);
-
-        setName("");
-        setEmail("");
-        setPassword("");
-        
-      } catch (error) {
-        if (error.response) {
-          if (error.response.status === 409) {
-            toast.info("User with provided credentials already exists");
-          }
-          console.error("Response data:", error.response.data);
-        } else {
-          toast.error("Form submission failed!");
-          console.error("Error:", error.message);
-        }
-      }
-    }
-  };
   return (
     <main className="flex min-h-screen">
       <Toast />
@@ -84,75 +15,8 @@ export default function SignUp() {
             Sign Up
           </h1>
 
-          <form className="w-full my-8">
-            <InputField
-              id="name"
-              label="Name"
-              type="text"
-              placeholder="John Doe"
-              value={name}
-              setter={setName}
-              mt="2"
-              mb="6"
-            />
-
-            <InputField
-              id="email"
-              label="Email"
-              type="email"
-              placeholder="johndoe@gmail.com"
-              value={email}
-              setter={setEmail}
-              mt="2"
-              mb="6"
-            />
-
-            <InputField
-              id="password"
-              label="Password"
-              type="password"
-              placeholder="********"
-              value={password}
-              setter={setPassword}
-              mt="1"
-              mb="6"
-            />
-
-            <div className="flex gap-4 mb-8 md:mb-12">
-              <input
-                type="checkbox"
-                id="acceptpolicy"
-                checked={isChecked}
-                onChange={handleCheckboxChange}
-                required
-                className="relative peer shrink-0 appearance-none w-4 h-4 border-2 border-black rounded-sm bg-white mt-1 checked:bg-black checked:border-0"
-              />
-              <label htmlFor="acceptpolicy" className="text-lg font-notosans">
-                I accept the terms and conditions
-              </label>
-              <svg
-                className="absolute w-4 h-4 mt-1 hidden peer-checked:block pointer-events-none"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="white"
-                strokeWidth="4"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <polyline points="20 6 9 17 4 12"></polyline>
-              </svg>
-            </div>
-
-            <button
-              className="block m-auto btn-primary"
-              type="submit"
-              onClick={handleSubmit}
-            >
-              Sign Up
-            </button>
-          </form>
-
+          <SignUpForm />
+          
           {/* Sign Up button for small screens*/}
           <p className="lg:hidden font-notosans text-lg font-medium text-center">
             If you already have an account,{" "}
@@ -177,7 +41,9 @@ export default function SignUp() {
             If you already have an account, Login Below
           </p>
           <Link href={"/login"}>
-            <button className="btn-primary bg-white text-black">Login</button>
+            <button className="btn-primary bg-white text-black hover:bg-black hover:text-white">
+              Login
+            </button>
           </Link>
         </div>
       </div>
