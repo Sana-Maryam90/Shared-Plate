124abdbeb5828397e48be2e29887350e59d72dae
Ayesha Imam <ayesha.imam786786@gmail.com>
2024-02-17 21:53:04 +0500

    added APIs for sign, login and verifying current logged in user

--- helper/db.js
+++ helper/db.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import User from "@/models/user";
 
 export const connectDb = async() => {
 
@@ -6,7 +7,16 @@ export const connectDb = async() => {
         const {connection} = await mongoose.connect(process.env.MONGO_DB_URI,{dbName:"SharedPlate"});
 
         console.log("db connected...");
-        console.log(connection);
+        //console.log(connection);
+
+        // const user = new User({
+        //     name:"maryam",
+        //     email:"maryam@gmail.com",
+        //     password:"ayesha123"
+        // })
+
+        // await user.save();
+        // console.log("**************user created******************");
 
     }
     catch(error){
