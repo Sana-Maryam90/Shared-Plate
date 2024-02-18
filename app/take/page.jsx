1233a84f76fd9a3539845d187eac3cc094fd0f2f
Sana-maraym90 <sanamaryam6677@gmail.com>
2024-02-18 14:10:59 +0500

    Modified Card component, added user location marker in Map component, added sample requests location markers in take page

--- app/take/page.jsx
+++ app/take/page.jsx
@@ -8,12 +8,38 @@ import { FaMapMarkerAlt } from "react-icons/fa";
 
 
 const Take = () => {
+    // sampleData.js
+const requests = [
+    {
+      name: "John Doe",
+      organization: "Community Kitchen",
+      foodtype: "Pasta",
+      location: { latitude: 24.91958236034102, longitude: 67.09738983659071 }
+    },
+    {
+      name: "Alice Smith",
+      organization: "Local Church",
+      foodtype: "Burgers",
+      location: { latitude: 24.951316863001107, longitude: 67.16312097762261 }
+    },
+    {
+      name: "Michael Johnson",
+      organization: "Food Bank",
+      foodtype: "Vegetarian",
+      location: { latitude: 25.04199986759563, longitude: 67.1528014153569 }
+    },
+    // Add more data here
+  ];
+    
+
+
+
   return (
 
     <main className="font-notoSans 2xl:flex xl:flex lg:flex md:flex">
         {/*big screens*/}
       <div className='w-[100%] md:w-[60%] 2xl:w-[70%] xl:w-[70%] lg:w-[70%]'>
-        <GoogleMap/>
+        <GoogleMap requests={requests}/>
         <div className=' bg-white text-black flex-row m-5 p-2'>
           <div className='flex justify-center items-center'>
             <FaCircleDot className='text-xl font-semibold' color='#9CA3AF' />
@@ -30,14 +56,20 @@ const Take = () => {
       </div>
 
       <div className='flex flex-col bg-green justify-center  items-center w-[100%] md:w-[40%] 2xl:w-[30%] xl:w-[30%] lg:w-[30%]'>
-        <Card/>
-        <Card/>
-        <Card/>
+      {requests.map((request, index) => (
+          <Card
+            key={index}
+            name={request.name}
+            organization={request.organization}
+            type={request.foodtype}
+          />
+        ))}
+        
       </div>  
     </main>
     
   )
 }
 
-export default Take
+export default Take;
 
