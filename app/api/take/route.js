48054e3cf8711218522b41a07e986b6468e4c256
Ayesha Imam <ayesha.imam786786@gmail.com>
2024-02-25 00:09:19 +0500

    minor changes

--- app/api/take/route.js
+++ app/api/take/route.js
@@ -70,6 +70,7 @@ export async function POST(request) {
                         <li style="margin-bottom: 5px; color: #666666;"><strong>Comment:</strong> ${comment}</li>
                         <li style="margin-bottom: 5px; color: #666666;"><strong>Location:</strong> <a href="${mapLink}" style="color: #007bff; text-decoration: none;">View Location on Map</a></li>
                     </ul>
+                    <button id="acceptRequestButton">Accept Request</button>
                 </div>
                 <p style="color: #666666;">Thank you for your generosity.</p>
                 <p style="color: #666666;">Best regards,<br><strong>Team SharedPlate</strong></p>
