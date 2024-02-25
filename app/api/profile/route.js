72c7738406a734bbc8bbca7a6e94de121bba4735
Ayesha Imam <ayesha.imam786786@gmail.com>
2024-02-25 20:47:22 +0500

    updated acceptRequest api to send taker an email, updated take API, profile and delete API

--- app/api/profile/route.js
+++ app/api/profile/route.js
@@ -1,12 +1,13 @@
 import { NextResponse } from 'next/server';
 import GiveRequest from '@/models/giveRequest';
+import Taker from '@/models/taker'; // Import the Taker model
 import { connectDb } from '@/helper/db';
 
 export async function POST(request) {
     try {
         await connectDb();
 
-        // Extract the userId from the request body
+        // Extract the userId from the request parameters
         const { userId } = await request.json();
 
         // Find all closed requests where the user acted as a giver
@@ -24,13 +25,36 @@ export async function POST(request) {
         // Find ongoing requests for the user as a giver or taker
         const ongoingRequests = await GiveRequest.find({
             $or: [
-                { giverId: userId, status: 'open' },
-                { volunteerId: userId, status: 'open' }
+                { giverId: userId, status: 'ongoing' },
+                { volunteerId: userId, status: 'ongoing' }
             ]
         });
 
+        // Find open requests with requestedBy field
+        const openRequestedBy = await GiveRequest.find({
+            giverId: userId,
+            status: 'open',
+            requestedBy: { $exists: true } // Ensure requestedBy field exists
+        });
+
+        // Extract user IDs from requestedBy field in open requests
+        const userIds = openRequestedBy.reduce((acc, curr) => {
+            acc.push(...curr.requestedBy);
+            return acc;
+        }, []);
+
+        // Find Takers based on user IDs from requestedBy field
+        const takersRequesting = await Taker.find({ userId: { $in: userIds } });
+
+        // Find open requests not requested by anyone
+        const openRequestsNotRequested = await GiveRequest.find({
+            giverId: userId,
+            status: 'open',
+            requestedBy: { $exists: false } // Ensure requestedBy field doesn't exist
+        });
+
         // Return the requests in the response
-        return NextResponse.json({ giverRequests, takerRequests, ongoingRequests }, { status: 200 });
+        return NextResponse.json({ giverRequests, takerRequests, ongoingRequests, takersRequesting, openRequestsNotRequested }, { status: 200 });
     } catch (error) {
         console.error('Error fetching user requests:', error.message);
         // Handle errors and return appropriate response
@@ -39,6 +63,61 @@ export async function POST(request) {
 }
 
 
+// import { NextResponse } from 'next/server';
+// import GiveRequest from '@/models/giveRequest';
+// import Taker from '@/models/taker'; // Import the Taker model
+// import { connectDb } from '@/helper/db';
+
+// export async function POST(request) {
+//     try {
+//         await connectDb();
+
+//         // Extract the userId from the request parameters
+//         const { userId } = await request.json();
+
+//         // Find all closed requests where the user acted as a giver
+//         const giverRequests = await GiveRequest.find({
+//             giverId: userId,
+//             status: 'closed'
+//         });
+
+//         // Find all closed requests where the user acted as a taker (volunteer)
+//         const takerRequests = await GiveRequest.find({
+//             volunteerId: userId,
+//             status: 'closed'
+//         });
+
+//         // Find ongoing requests for the user as a giver or taker
+//         const ongoingRequests = await GiveRequest.find({
+//             $or: [
+//                 { giverId: userId, status: 'ongoing' },
+//                 { volunteerId: userId, status: 'ongoing' }
+//             ]
+//         });
+
+//         // Find open requests with requestedBy field
+//         const openRequestedBy = await GiveRequest.find({
+//             status: 'open',
+//             requestedBy: { $exists: true } // Ensure requestedBy field exists and is not empty
+//         });
+
+//         // Extract user IDs from requestedBy field in open requests
+//         const userIds = openRequestedBy.reduce((acc, curr) => {
+//             acc.push(...curr.requestedBy);
+//             return acc;
+//         }, []);
+
+//         // Find Takers based on user IDs from requestedBy field
+//         const takersRequesting = await Taker.find({ userId: { $in: userIds } });
+
+//         // Return the requests in the response
+//         return NextResponse.json({ giverRequests, takerRequests, ongoingRequests, takersRequesting }, { status: 200 });
+//     } catch (error) {
+//         console.error('Error fetching user requests:', error.message);
+//         // Handle errors and return appropriate response
+//         return NextResponse.json({ message: error.message }, { status: 500 });
+//     }
+// }
 
 
 // import { NextResponse } from 'next/server';
@@ -52,30 +131,30 @@ export async function POST(request) {
 //         // Extract the userId from the request body
 //         const { userId } = await request.json();
 
-//         // Count how many times the user acted as a giver in closed requests
-//         const giverCount = await GiveRequest.countDocuments({
+//         // Find all closed requests where the user acted as a giver
+//         const giverRequests = await GiveRequest.find({
 //             giverId: userId,
 //             status: 'closed'
 //         });
 
-//         // Count how many times the user acted as a taker (volunteer) in closed requests
-//         const takerCount = await GiveRequest.countDocuments({
+//         // Find all closed requests where the user acted as a taker (volunteer)
+//         const takerRequests = await GiveRequest.find({
 //             volunteerId: userId,
 //             status: 'closed'
 //         });
 
-//         // Check if there are ongoing requests for the user as a giver or taker
-//         const ongoingRequests = await GiveRequest.countDocuments({
+//         // Find ongoing requests for the user as a giver or taker
+//         const ongoingRequests = await GiveRequest.find({
 //             $or: [
-//                 { giverId: userId, status: 'open' },
-//                 { volunteerId: userId, status: 'open' }
+//                 { giverId: userId, status: 'ongoing' },
+//                 { volunteerId: userId, status: 'ongoing' }
 //             ]
 //         });
 
-//         // Return the counts and ongoing requests in the response
-//         return NextResponse.json({ giverCount, takerCount, ongoingRequests }, { status: 200 });
+//         // Return the requests in the response
+//         return NextResponse.json({ giverRequests, takerRequests, ongoingRequests }, { status: 200 });
 //     } catch (error) {
-//         console.error('Error counting user actions:', error);
+//         console.error('Error fetching user requests:', error.message);
 //         // Handle errors and return appropriate response
 //         return NextResponse.json({ message: error.message }, { status: 500 });
 //     }
--- models/taker.js
+++ models/taker.js
@@ -4,6 +4,7 @@ const takerSchema = new Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     name: { type: String, required: true },
     contact: { type: String, required: true },
+    comment: { type: String},
     location: {
         latitude: { type: Number, required: false },  // will make it required after adding sana's code
         longitude: { type: Number, required: false }
