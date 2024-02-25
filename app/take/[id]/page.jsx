2b365677ebe164af46af5528cedc86f3a495359b
Sana-maraym90 <sanamaryam6677@gmail.com>
2024-02-25 11:15:24 +0500

    Correction for delete request

--- app/take/[id]/page.jsx
+++ app/take/[id]/page.jsx
@@ -16,24 +16,8 @@ async function getRequest(id) {
 }
 
 
-async function deleteOverdueGiveRequests() {
-  try {
-      const response = await fetch('http://localhost:3000/api/deleteRequest', {
-          method: 'POST',
-      });
-      const data = await response.json();
-      console.log(data); // Log the response from the server
-  } catch (error) {
-      console.error('Error deleting overdue giveRequests:', error);
-  }
-}
-
-
-
 
 export default async function TakeRequest({params}) {
-
-  await deleteOverdueGiveRequests();
 
 
   const id = params.id;
